a0a69918710ffc59e80b87438ab0e7f601730197
Cesar Jose Rodriguez Diaz <cesarjgrd.18@gmail.com>
2017-10-01 17:05:51 -0400

    unknow

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,14 +1,27 @@
 import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
 
 import {
-    FACEBOOK_LOGIN_SUCCESS
+    FACEBOOK_LOGIN_SUCCESS,
+    FACEBOOK_LOGIN_FAIL
 } from './types.js';
 
 export const facebookLogin = () => async dispatch => {
     let token = await AsyncStorage.getItem('fb_login');
     if (token) {
-        //dispatch action FB is login
+        dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
     } else {
-        // start fb login process
+        doFacebookLogin(dispatch);
     }
+};
+
+const doFacebookLogin = async dispatch => {
+    let { type, token } = await Facebook.logInWithReadPermissionsAsync('130248560949685', {
+        permissions: ['public_profile']
+    });
+
+    if (type === 'cancel') return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+    
+    await AsyncStorage.setItem('fb_login', token);
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
 };
